f296de6d5fbe09f12e4963644b6d0ab6a3909850
Milo Knowles <miloknowles97@gmail.com>
2024-03-01 15:33:23 -0500

    WIP

--- classifier/arxiv_dataset.py
+++ classifier/arxiv_dataset.py
@@ -1,60 +1,38 @@
 import json
 
-from torch.utils.data import IterableDataset as _BaseIterableDataset
-from datasets import IterableDataset, IterableDatasetDict, Dataset
+from datasets import IterableDataset, IterableDatasetDict
 
 
-
-def create_dataset(jsonl_path: str) -> IterableDataset:
+def create_iterable_dataset(jsonl_path: str) -> IterableDataset:
   """Creates an iterable dataset from a path to a JSONL file.
   
+  Notes
+  -----
   This avoids reading the entire file into memory at once.
+  
+  The limitation of iterable datasets is that their length isn't known ahead of
+  time. If you don't specify a `max-steps` argument in the `TrainingArguments`,
+  you'll get an error about the learning rate scheduler.
+
+  In the future, we may want to see if __len__ can be overridden to get the
+  best of both worlds.
+  ```
   """
   def _generator():
     with open(jsonl_path, 'r') as f:
       for line in f:
         yield json.loads(line)
-  
-  # Get the features from the first example.
-  # TODO(milo): Should we infer the features here?
-  # features = list(next(_generator()).keys())
 
   return IterableDataset.from_generator(_generator)
 
 
-# class ArxivDataset(_BaseIterableDataset):
-#   def __init__(
-#     self,
-#     jsonl_path: str
-#   ):
-#     super(_BaseIterableDataset, self).__init__()
-#     self._path = jsonl_path
-
-#     with open(self._path, 'r') as f:
-#       for count, line in enumerate(f):
-#         pass
-
-#     self._length = count + 1
-
-#   def __iter__(self):
-#     with open(self._path, 'r') as f:
-#       for line in f:
-#         yield json.loads(line)
-
-#   def __len__(self) -> int:
-#     return self._length
-  
-
-def create_dataset_dict(
+def load_dataset_splits(
   train_path: str, val_path: str, test_path: str
 ) -> IterableDatasetDict:
   """Creates a `DatasetDict` from paths to JSONL split files."""
   return IterableDatasetDict({
-    "train": create_dataset(train_path),
-    "val": create_dataset(val_path),
-    "test": create_dataset(test_path),
-    # "train": ArxivDataset(train_path),
-    # "val": ArxivDataset(val_path),
-    # "test": ArxivDataset(test_path),
+    "train": create_iterable_dataset(train_path),
+    "val": create_iterable_dataset(val_path),
+    "test": create_iterable_dataset(test_path),
   })
 
--- classifier/finetune.py
+++ classifier/finetune.py
@@ -3,12 +3,11 @@ import logging
 
 import torch
 
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-from transformers import TrainingArguments
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, DataCollatorWithPadding
 from transformers import TrainingArguments, Trainer
 
 from datasets import load_dataset, DatasetDict
-from classifier.arxiv_dataset import create_dataset_dict
+from classifier.arxiv_dataset import load_dataset_splits
 from classifier.paths import data_folder, models_folder
 
 import numpy as np
@@ -28,36 +27,43 @@ metrics = dict(
 def compute_metrics(pred_eval: tuple[torch.Tensor, torch.Tensor]) -> dict[str, float]:
   """Takes a tuple of logits and labels and returns a dictionary of metrics."""
   logits, labels = pred_eval
-  print(logits.shape, labels.shape)
-  print(logits)
-  print(labels)
   predictions = np.argmax(logits, axis=-1)
   return {name: metric.compute(predictions=predictions, references=labels) for name, metric in metrics.items()}
 
 
-def get_max_steps(train_path: str) -> int:
+def get_max_steps(train_path: str, num_train_epochs: int, batch_size: int) -> int:
   """Get the maximum number of training steps.
   
   This is required for the `TrainingArguments` object, since we're using an
-  iterable dataset that is backed by a generator.
+  iterable dataset that is backed by a generator with unknown length.
   """
   with open(train_path, 'r') as f:
-    for count, _ in enumerate(f):
+    for n_examples, _ in enumerate(f):
       pass
-  return count
+  return (n_examples + 1) * num_train_epochs // batch_size
 
 
 def main():
   num_labels = 2
-  model_name = 'albert-base-v2'
+  num_train_epochs = 10
+  batch_size = 32
+
+  model_name = "distilbert/distilbert-base-uncased"
   run_name = "debugging"
 
-  model = AutoModelForSequenceClassification.from_pretrained(
-    model_name, num_labels=num_labels
-  )
-  tokenizer = AutoTokenizer.from_pretrained(model_name)
+  id2label = {0: "False", 1: "True"}
+  label2id = {"False": 0, "True": 1}
 
-  dataset = create_dataset_dict(
+  model = AutoModelForSequenceClassification.from_pretrained(
+    model_name,
+    num_labels=num_labels,
+    id2label=id2label,
+    label2id=label2id,
+  )
+  # https://stackoverflow.com/questions/69842980/asking-to-truncate-to-max-length-but-no-maximum-length-is-provided-and-the-model
+  tokenizer = AutoTokenizer.from_pretrained(model_name, model_max_length=512)
+
+  dataset = load_dataset_splits(
     data_folder / "finetuning" / "train.jsonl",
     data_folder / "finetuning" / "val.jsonl",
     data_folder / "finetuning" / "test.jsonl",
@@ -69,20 +75,20 @@ def main():
 
   def tokenize(examples: dict[str, list[int | str]]):
     """Tokenize the `text` field of all examples."""
-    return tokenizer(examples["text"], padding="max_length", truncation=True)
+    return tokenizer(examples["text"], truncation=True, padding="max_length")
 
   dataset = dataset.map(convert_labels, batched=True)
-  dataset = dataset.map(tokenize, batched=True)
+  dataset = dataset.map(tokenize, batched=True).shuffle(seed=42)
 
   training_args = TrainingArguments(
     output_dir=models_folder / run_name,
     evaluation_strategy="epoch",
-    num_train_epochs=10,
+    num_train_epochs=num_train_epochs,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
     save_strategy="epoch",
-    max_steps=get_max_steps(data_folder / "finetuning" / "train.jsonl"),
+    max_steps=get_max_steps(data_folder / "finetuning" / "train.jsonl", num_train_epochs, batch_size),
   )
-
-  print(training_args)
 
   trainer = Trainer(
     model=model,
@@ -90,6 +96,8 @@ def main():
     train_dataset=dataset["train"],
     eval_dataset=dataset["val"],
     compute_metrics=compute_metrics,
+    # data_collator=data_collator,
+    tokenizer=tokenizer,
   )
 
   trainer.train()
